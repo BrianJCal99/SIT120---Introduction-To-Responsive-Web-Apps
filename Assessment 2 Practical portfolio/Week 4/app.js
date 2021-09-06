4216158a89d7f128ee51541ae898a7a4eff4cd91
Brian Caldera <88503893+BrianJCal99@users.noreply.github.com>
2021-09-06 16:04:25 +0530

    Add files via upload

---
+++ Assessment 2 Practical portfolio/Week 4/app.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    greeting: 'Hi!',
+    message: 'This is my very first Vue app',
+    x: true,
+    y: false,
+    WhatWeLearnedSoFar: [
+      {item:'week1', text:'HTML'},
+      {item: 'week2', text:'CSS'},
+      {item: 'week3', text:'JS'},
+      {item: 'week4', text:'Vue.js'}
+    ],
+  }
+})
